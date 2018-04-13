3ae894db438f1221163ff0795f8f903be9352ba5
fabianmedina09 <fabianmedina09@gmail.com>
2018-04-13 15:07:47 -0500

    ADMIN AND MODELS CONFIG

--- posts/admin.py
+++ posts/admin.py
@@ -4,3 +4,35 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from posts.models import Herramienta, Tutorial, Actividad, Perfil
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+
+class ProfileInline(admin.StackedInline):
+    model = Perfil
+    can_delete = False
+    verbose_name_plural = 'Perfil'
+    fk_name = 'user'
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'get_rol')
+
+    def get_rol(self, instance):
+        return instance.perfil.role
+
+    get_rol.short_description = 'Rol'
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+
+admin.site.register(Herramienta)
+admin.site.register(Tutorial)
+admin.site.register(Actividad)
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- posts/models.py
+++ posts/models.py
@@ -10,7 +10,11 @@ from django.dispatch import receiver
 class TestModel(models.Model):
     nombre = models.CharField(max_length=100, null=False, blank=False)
 
+
 class Herramienta(models.Model):
+    class Meta:
+        unique_together = (('id', 'version'),)
+    version = models.CharField(max_length=100, null=False, blank=False)
     nombre = models.CharField(max_length=100, null=False, blank=False)
     urlReferencia = models.CharField(max_length=500, null=False, blank=False)
     sistemaOperativo = models.CharField(max_length=50, null=False, blank=False)
@@ -57,3 +61,67 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         Perfil.objects.create(user=instance)
     instance.perfil.save()
+
+
+class RecursoActividad(models.Model):
+    url = models.CharField(max_length=200, null=False, blank=False)
+    descripcion = models.CharField(max_length=500, null=False, blank=False)
+
+
+class Actividad(models.Model):
+    class Meta:
+        unique_together = (('id', 'version'),)
+    version = models.CharField(max_length=100, null=False, blank=False)
+    nombre = models.CharField(max_length=100, null=False, blank=False)
+    descripcion = models.CharField(max_length=500, null=False, blank=False)
+    instrucciones = models.CharField(max_length=500, null=False, blank=False)
+    url = models.CharField(max_length=200, null=True, blank=True)
+    PENDIETE_REVISION = 1
+    PENDIENTE_PUBLICACION = 2
+    PUBLICADO = 3
+    BLOQUEADO = 4
+    ESTADO_CHOICES = (
+        (PENDIETE_REVISION, 'Pendiente de Revisión'),
+        (PENDIENTE_PUBLICACION, 'Pendiente de Publicación'),
+        (PUBLICADO, 'Publicado'),
+        (BLOQUEADO, 'Bloqueado'),
+    )
+    estado = models.PositiveSmallIntegerField(choices=ESTADO_CHOICES, null=True, blank=True)
+    revisiones = models.IntegerField(null=False, blank=False)
+    recurso = models.ManyToManyField(RecursoActividad, blank=False, through='RecursoActividad_Actividad')
+
+
+class RecursoActividad_Actividad(models.Model):
+    recursoActividad = models.ForeignKey(RecursoActividad, on_delete=models.CASCADE)
+    actividad = models.ForeignKey(Actividad, on_delete=models.CASCADE)
+
+
+class RecursoTutorial(models.Model):
+    url = models.CharField(max_length=200, null=False, blank=False)
+    descripcion = models.CharField(max_length=500, null=False, blank=False)
+
+
+class Tutorial(models.Model):
+    class Meta:
+        unique_together = (('id', 'version'),)
+    version = models.CharField(max_length=100, null=False, blank=False)
+    nombre = models.CharField(max_length=100, null=False, blank=False)
+    funcionalidad = models.CharField(max_length=500, null=False, blank=False)
+    PENDIETE_REVISION = 1
+    PENDIENTE_PUBLICACION = 2
+    PUBLICADO = 3
+    BLOQUEADO = 4
+    ESTADO_CHOICES = (
+        (PENDIETE_REVISION, 'Pendiente de Revisión'),
+        (PENDIENTE_PUBLICACION, 'Pendiente de Publicación'),
+        (PUBLICADO, 'Publicado'),
+        (BLOQUEADO, 'Bloqueado'),
+    )
+    estado = models.PositiveSmallIntegerField(choices=ESTADO_CHOICES, null=True, blank=True)
+    revisiones = models.IntegerField(null=False, blank=False)
+    recurso = models.ManyToManyField(RecursoTutorial, blank=False, through='RecursoTutorial_Tutorial')
+
+
+class RecursoTutorial_Tutorial(models.Model):
+    recursoTutorial = models.ForeignKey(RecursoTutorial, on_delete=models.CASCADE)
+    tutorial = models.ForeignKey(Tutorial, on_delete=models.CASCADE)

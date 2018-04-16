05baee837df5f8cd23134adb2683f1d459618754
Pablo Alvarado <pa.alvarado10@uniandes.edu.co>
2018-04-15 21:08:15 -0500

    comentarios

--- posts/automaticTest.py
+++ posts/automaticTest.py
@@ -1,15 +1,30 @@
 # -- coding: utf-8 --
 from unittest import TestCase
+
+import sys
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.by import By
 
 
 class FunctionalTest(TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome("C:\\Users\\chromedriver.exe")
     # Sebastian añada su ruta por tener mac :c, para el chromedriver
+        self.browser.implicitly_wait(2)
 
-    def test_title(self):
+    def test_1_title(self):
         self.browser.get('localhost:8000')
         self.assertIn('Home CatalogoCTE', self.browser.title)
-        self.browser.implicitly_wait(100)
+        self.browser.close()
+    #
+    # def test_2_login_admin(self):
+    #     self.browser.get('localhost:8000')
+    #     self.browser.find_element_by_id('id_login').click()
+    #     nombreUsuario = self.browser.find_element_by_name('username')
+    #     nombreUsuario.send_keys('admin')
+    #     clave = self.browser.find_element_by_id('password')
+    #     clave.send_keys('catalogo2018')
+    #     self.browser.find_element_by_id('id_entrar').click()
+    #     self.browser.implicitly_wait(10)
+    #     aref = self.browser.find_element_by_xpath('//a[@href="logout"]')
+    #     self.assertIn('Logout', aref.text)

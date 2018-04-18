e3b8bd7aa6e657abd9e0451e7eafc529d75bb7c9
Pablo Alvarado <pa.alvarado10@uniandes.edu.co>
2018-04-17 19:32:48 -0500

    pruebas parciales 2

--- posts/automaticTest.py
+++ posts/automaticTest.py
@@ -66,8 +66,8 @@ class FunctionalTest(TestCase):
     #     clave.send_keys('catalogo2018')
     #     self.browser.find_element_by_id('boton_login').click()
     #     aref = self.browser.find_element_by_xpath('//a[@href="/herramienta_update/491"]').click()
-    #     nombre_herramienta = self.browser.find_element_by_id('id_descripcion')
-    #     nombre_herramienta.send_keys('Sistema para el manejo de la informacion de los estudiantes y sus cursos')
+    #     descripcion_herramienta = self.browser.find_element_by_id('id_descripcion')
+    #     descripcion_herramienta.send_keys('Sistema para el manejo de la informacion de los estudiantes y sus cursos')
     #     self.browser.find_element_by_id('boton_actualizar_herramienta').click()
     #
     #     p = self.browser.find_element(By.XPATH, '//p[text()="Sistema para el manejo de la informacion de los estudiantes y sus cursos"]')
@@ -116,4 +116,64 @@ class FunctionalTest(TestCase):
     #     aref = self.browser.find_element_by_xpath('//li[text="usuario prueba  --- Miembro GTI"]')
     #     self.assertIn('usuario prueba  --- Miembro GTI', aref.text)
     #
+    # def test_7_login_miembro_gti(self):
+    #     self.browser.get('https://catalogodevelop.herokuapp.com/')
+    #     self.browser.find_element_by_id('id_login').click()
+    #     nombre_usuario = self.browser.find_element_by_name('username')
+    #     nombre_usuario.send_keys('juan')
+    #     clave = self.browser.find_element_by_id('password')
+    #     clave.send_keys('sebas2018')
+    #     self.browser.find_element_by_id('boton_login').click()
+    #     aref = self.browser.find_element_by_xpath('//h3[text="Bienvenido Sebastian Barragan Miembro GTI"]')
+    #     self.assertIn('Bienvenido Sebastian Barragan Miembro GTI', aref.text)
     #
+    #
+    # def test_8_agregar_herramienta(self):
+    #     self.browser.get('localhost:8000')
+    #     self.browser.find_element_by_id('id_login').click()
+    #     nombre_usuario = self.browser.find_element_by_name('username')
+    #     nombre_usuario.send_keys('juan')
+    #     clave = self.browser.find_element_by_id('password')
+    #     clave.send_keys('sebas2018')
+    #     self.browser.find_element_by_id('boton_login').click()
+    #     self.browser.find_element_by_id('boton_agregar_herramienta').click()
+    #     nombre_herramienta = self.browser.find_element_by_id('id_nombre')
+    #     nombre_herramienta.send_keys("Herramienta Test Miembro GTI")
+    #     url_herramienta = self.browser.find_element_by_id('id_urlReferencia')
+    #     url_herramienta.send_keys("www.testHerramienta.com")
+    #     sistema_operativo_herramienta = self.browser.find_element_by_id('id_sistemaOperativo')
+    #     sistema_operativo_herramienta.send_keys("Windows")
+    #     plataforma_herramienta = self.browser.find_element_by_id('id_plataforma')
+    #     plataforma_herramienta.send_keys("Plataforma Test")
+    #     ficha_tecnica_herramienta = self.browser.find_element_by_id('id_fichaTecnica')
+    #     ficha_tecnica_herramienta.send_keys("Ficha Tecnica Test")
+    #     licencia_herramienta = self.browser.find_element_by_id('id_licencia')
+    #     licencia_herramienta.send_keys("Ficha Tecnica Test")
+    #     descripcion_herramienta = self.browser.find_element_by_id('id_descripcion')
+    #     descripcion_herramienta.send_keys("Descripcion Test creada por Miembro GTI")
+    #     self.browser.find_element_by_id('boton_add').click()
+    #
+    #     p = self.browser.find_element(By.XPATH, '//p[text()="Descripcion Test creada por Miembro GTI"]')
+    #     self.assertIn('Descripcion Test creada por Miembro GTI', p.text)
+    #
+    #
+    # def test_9_editar_herramienta_miembro_gti(self):
+    #     self.browser.get('localhost:8000')
+    #     self.browser.find_element_by_id('id_login').click()
+    #     nombre_usuario = self.browser.find_element_by_name('username')
+    #     nombre_usuario.send_keys('juan')
+    #     clave = self.browser.find_element_by_id('password')
+    #     clave.send_keys('sebas2018')
+    #     self.browser.find_element_by_id('boton_login').click()
+    #     aref = self.browser.find_element_by_xpath('//a[@href="/herramienta_update/491"]').click()
+    #     descripcion_herramienta = self.browser.find_element_by_id('id_descripcion')
+    #     descripcion_herramienta.send_keys('Sistema para el manejo de la informacion de los estudiantes y sus cursos (prueba)')
+    #     self.browser.find_element_by_id('boton_actualizar_herramienta').click()
+    #
+    #     p = self.browser.find_element(By.XPATH, '//p[text()="Sistema para el manejo de la informacion de los estudiantes y sus cursos (prueba)"]')
+    #     self.assertIn('Sistema para el manejo de la informacion de los estudiantes y sus cursos (prueba)', p.text)
+    #
+    # def test_10_visualizacion_herramienta_usuario_cte(self):
+    #     self.browser.get('localhost:8000')
+    #     aref = self.browser.find_element_by_name('boton_detalle')
+    #     self.assertIn('Ver detalle', aref.text)
